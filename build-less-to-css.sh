79d0e5d62d5b69e149d93f2725dcdca9c92357dd
Sébastien Mennetrier <mennetrier@smart4.io>
2022-06-15 17:08:14 +0200

    Add smart4life theme and remove wiki image

--- build-less-to-css.sh
+++ build-less-to-css.sh
@@ -7,4 +7,7 @@ lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=.
 cd ../../clean-bordered/less
 lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
 cd ../../clean-dark/less
+lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
+cd ../../clean-smart4life/less
+lessc ./build.less ../css/theme.css --clean-css="--s1 --advanced" --source-map=../css/theme.css.map
 cd ../../..
